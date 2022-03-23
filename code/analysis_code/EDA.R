d45c6870cf9f77581a2f41ddccaefe35aa24ac27
Sophia <spd63133@uga.edu>
2022-03-23 13:22:44 -0400

    Added corr. plot

--- code/analysis_code/EDA.R
+++ code/analysis_code/EDA.R
@@ -44,7 +44,9 @@ demo1 <- demo %>% mutate_if(is.character,as.factor)
 str(covid1$State) 
 str(covid1$County) 
 summary(covid1$State)
-summary(covid1$County)
+# removing names
+covid2 <- covid1[!(covid1$State=="Guam" | covid1$State=="Grand Princess" | covid1$State=="American Somoa" | 
+                 covid1$State=="Diamond Princess"| covid1$State=="Northern Mariana Islands"| covid1$State=="Virgin Islands"),]
 
 str(demo1$State) 
 str(demo1$County) 
@@ -61,7 +63,7 @@ demo1 <- demo1 %>% select(-c("County",
 ################################# this needs work #############################
 # merging datasets ------------------------------------------------------------
 ## Adding demo to covid set because covid reported the largest amount of states and counties
-try1 <- merge(covid1,demo1,by=c("State","FIPS"), all = TRUE)
+try1 <- merge(covid2,demo1,by=c("State","FIPS"), all = TRUE)
 # now adding AQI to data set
 full <- merge(try1, aqi,by=c("State","County"), all = TRUE)
 
@@ -86,6 +88,12 @@ fullper <- fullper %>%
     rother = other/TotalPop,
     rmult_race = mult_race/TotalPop,
     rhisp  = hisp/TotalPop)
+summary(fullper$State)
+
+#fullper <- fullper[!(covid1$State=="Guam" | covid1$State=="Grand Princess" | covid1$State=="American Somoa" | 
+                     #covid1$State=="Diamond Princess"| covid1$State=="Northern Mariana Islands"| covid1$State=="Virgin Islands"),]
+
+
 
 # mapping at deaths per cases by county
 fullper <- rename(fullper, fips = FIPS)
@@ -130,8 +138,7 @@ caseratecountymap2 <- dataall %>%
   coord_map() + 
   scale_fill_continuous( low = "blue", high = "red", 
                                        name = "Cumulative Case rate Per County")
-caseratecountymap2 +
-theme_bw() +
+caseratecountymap2 + theme_bw() +
   theme(axis.line = element_line(colour = "white"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -142,54 +149,45 @@ caseratecountymap2
 #save
 ggsave(caseratecountymap2, filename = here("results", "caseratecountymap2.png"))
 
-untouched <- State, County, fips 
+# correlation plot
+library(corrplot)
+library("Hmisc")
+fullpermat <- fullper %>% select(av.aqi:rhisp) #rCumulativeCase,rCumulativeDeath, rTotalPop, male, female, under_18, age18to34, age35to64, age65, not_hispanic, white, black, AIANA, asian, pacific_island, other, mult_race, hisp, av.aqi)
+fullpermat.cor <- Hmisc::rcorr(na.omit(as.matrix(fullpermat)))
+corrplot(fullpermat.cor$r, type="upper", order="hclust", 
+         p.mat = fullpermat.cor$P, sig.level = 0.05, insig = "blank")
+#save
+ggsave(fullpermat.cor, filename = here("results", "fullpermat.cor.png"))
 
 
-# creating basic table by STATE ----does not run
-fullperstate<- full
+# creating basic table by STATE 
+fullperstate<- full 
 fullperstate <- fullperstate %>%
   group_by(State) %>% 
-  summarise(across(c(CumulativeCase,CumulativeDeath, TotalPop, male, female, under_18, age18to34, age35to64, age65, not_hispanic, white, black, AIANA, asian, pacific_island, other, mult_race, hisp), sum))
+  summarise(  
+  TotalPopulation = sum(TotalPop, na.rm = T),
+  SCumulativeCase = sum(CumulativeCase, na.rm = T), 
+  SCumulativeDeath = sum(CumulativeDeath, na.rm = T),
+  DeathPerCase = (SCumulativeDeath/SCumulativeCase)*100,
+  DeathRate = SCumulativeDeath/TotalPopulation, #dont like this, looks funky and possibly misleading.
+  Male = (sum(male, na.rm = T)/TotalPopulation)*100,
+  Under_18 = (sum(under_18, na.rm = T)/TotalPopulation)*100,
+  Age18to34 = (sum(age18to34, na.rm = T)/TotalPopulation)*100, 
+  Age35to64 = (sum(age35to64, na.rm = T)/TotalPopulation)*100,  
+  Age65 = (sum(age65, na.rm = T)/TotalPopulation)*100,  
+  Not_hispanic = (sum(not_hispanic, na.rm = T)/TotalPopulation)*100,  
+  White  = (sum(white, na.rm = T)/TotalPopulation)*100, 
+  Black = (sum(black, na.rm = T)/TotalPopulation)*100, 
+  AIANA = (sum(AIANA, na.rm = T)/TotalPopulation)*100,   
+  Asian = (sum(asian, na.rm = T)/TotalPopulation)*100,   
+  Pacific_island = (sum(pacific_island, na.rm = T)/TotalPopulation)*100,   
+  Other = (sum(other, na.rm = T)/TotalPopulation)*100,   
+  Mult_race = (sum(mult_race, na.rm = T)/TotalPopulation)*100,     
+  Hispanic = (sum(hisp, na.rm = T)/TotalPopulation)*100,
+  AQI = mean(av.aqi,na.rm = T))    
 
-  summarise(across(vars(CumulativeCase:hisp), sum))
-
-  summarise_at(which(sapply(is.numeric) & names(df) != 'Registered'), sum)
-  summarise(across(everything(), list(sum)))
-    
-  t = fullperstate %>% group_by(State) %>%
-    summarise_at(vars(CumulativeCase:hisp), fun=sum))
-                   
-    CumulativeCase = sum(CumulativeCase), 
-            CumulativeDeath = sum(CumulativeDeath),
-            deathpercase = sCumulativeDeath/CumulativeCase
-    rcase = CumulativeCase/TotalPop,
-    rdeath = CumulativeDeath/TotalPop,
-    rdeathpercase = CumulativeDeath/CumulativeCase,
-    rmale = male/TotalPop,
-    rfemale = female/TotalPop,
-    runder_18 = under_18/TotalPop,
-    rage18to34 = age18to34/TotalPop,
-    rage35to64 = age35to64/TotalPop,
-    rage65 = age65/TotalPop,
-    rnot_hispanic  = not_hispanic/TotalPop,
-    rwhite = white/TotalPop,
-    rblack = black/TotalPop,
-    rAIANA  = AIANA/TotalPop,    
-    rasian = asian/TotalPop,
-    rpacific_island = pacific_island/TotalPop,
-    rother = other/TotalPop,
-    rmult_race = mult_race/TotalPop,
-    rhisp  = hisp/TotalPop)
-  
-gtsummary::set_gtsummary_theme(my_theme)
-demofull <- fullperstate %>%
-  tbl_summary(by = State) %>% 
-  bold_labels() %>%
-  modify_header(label ~ "**Variable**") %>%
-  add_overall()
-demofull
-str(full)
-
+# copy and paste into excel
+fullperstate
 
 
 
